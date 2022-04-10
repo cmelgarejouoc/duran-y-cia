9d4c8180355e35066599601e414fe692625fbf29
Carles Melgarejo <cmelgarejo@uoc.edu>
2022-04-10 18:11:27 +0200

    Correcció bug array sort per chrome

--- src/js/app.js
+++ src/js/app.js
@@ -15,9 +15,6 @@ document.addEventListener('click', function (event) {
 	if (!event.target.closest('.close')) return;
 
 	document.querySelector(".hamburger-menu").classList.add("collapse-menu");
-	setTimeout(function(){
-		document.querySelector(".hamburger-menu").classList.remove("collapse-menu");
-	}, 3000);
 });
 
 /*
