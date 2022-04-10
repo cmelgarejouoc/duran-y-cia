53206d179fd3e8ce8662c00a1c846d7d5c738024
Carles Melgarejo <cmelgarejo@uoc.edu>
2022-04-10 21:02:21 +0200

    Validacions w3.org, i accessibilitat

--- src/js/app.js
+++ src/js/app.js
@@ -103,7 +103,7 @@ function renderVideosBy(field = 'date', order = 'asc'){
 	let nodes = videos.map(video => {
 		let li = document.createElement('li');
 		li.innerHTML= `
-		<iframe width="560" height="315" src="https://www.youtube.com/embed/` + video.yt_id + `" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+		<iframe width="560" height="315" src="https://www.youtube.com/embed/` + video.yt_id + `" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 		<p>` + video.title + ` <time datetime="` + video.date + `">` + video.date + `</time></p>`;
 		return li;
 	});
